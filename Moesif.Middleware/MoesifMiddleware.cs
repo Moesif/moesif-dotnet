af0b3d7cf30a4adc5dd2fb15b018f6f2f934f4bf
Keyur <keyur@moesif.com>
2018-11-15 11:18:43 -0800

    Initial Commit
    Initial Commit

---
+++ Moesif.Middleware/MoesifMiddleware.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Moesif.Api;
+using Moesif.Api.Models;
+using Moesif.Api.Exceptions;
+using Moesif.Api.Http.Client;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace Moesif.Middleware
+{
+    public class MoesifMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public string userId;
+
+        public string sessionToken;
+
+        public Dictionary<string, object> metadata;
+
+        public Dictionary<string, object> moesifOptions;
+
+        public bool debug;
+
+        public bool Debug()
+        {
+            bool localDebug;
+            var debug_out = new object();
+            var getDebug = moesifOptions.TryGetValue("LocalDebug", out debug_out);
+            if (getDebug)
+            {
+                localDebug = Convert.ToBoolean(debug_out);
+            }
+            else
+            {
+                localDebug = false;
+            }
+            return localDebug;
+        }
+
+        public MoesifMiddleware(RequestDelegate next, Dictionary<string, object> _middleware)
+        {
+            moesifOptions = _middleware;
+            debug = Debug();
+            _next = next;
+        }
+
+        public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext httpContext)
+        {
+            var request = await FormatRequest(httpContext.Request);
+
+            var originalBodyStream = httpContext.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                httpContext.Response.Body = responseBody;
+
+                await _next(httpContext);
+
+                var response = await FormatResponse(httpContext.Response);
+
+                // User Id 
+                var user_out = new object();
+                var getUserId = moesifOptions.TryGetValue("IdentifyUser", out user_out);
+
+                Func<HttpRequest, HttpResponse, string> IdentifyUser = null;
+                if (getUserId)
+                {
+                    IdentifyUser = (Func<HttpRequest, HttpResponse, string>)(user_out);
+                }
+
+                // SessionToken
+                var token_out = new object();
+                var getSessionToken = moesifOptions.TryGetValue("GetSessionToken", out token_out);
+
+                Func<HttpRequest, HttpResponse, string> GetSessionToken = null;
+                if (getSessionToken)
+                {
+                    GetSessionToken = (Func<HttpRequest, HttpResponse, string>)(token_out);
+                }
+
+                // Get Metadata
+                var metadata_out = new object();
+                var getMetadata = moesifOptions.TryGetValue("GetMetadata", out metadata_out);
+
+                Func<HttpRequest, HttpResponse, Dictionary<string, object>> GetMetadata = null;
+
+                if (getMetadata)
+                {
+                    GetMetadata = (Func<HttpRequest, HttpResponse, Dictionary<string, object>>)(metadata_out);
+                }
+
+                // UserId
+                userId = null;
+                if (IdentifyUser != null)
+                {
+                    userId = IdentifyUser(httpContext.Request, httpContext.Response);
+                }
+
+                // Session Token
+                sessionToken = null;
+                if (GetSessionToken != null)
+                {
+                    sessionToken = GetSessionToken(httpContext.Request, httpContext.Response);
+                }
+
+                // Metadata
+                metadata = null;
+                if (GetMetadata != null)
+                {
+                    metadata = GetMetadata(httpContext.Request, httpContext.Response);
+                }
+
+                // Get Skip
+                var skip_out = new object();
+                var getSkip = moesifOptions.TryGetValue("Skip", out skip_out);
+
+                // Check to see if we need to send event to Moesif
+                Func<HttpRequest, HttpResponse, bool> ShouldSkip = null;
+
+                if (getSkip)
+                {
+                    ShouldSkip = (Func<HttpRequest, HttpResponse, bool>)(skip_out);
+                }
+
+                if (ShouldSkip != null)
+                {
+                    if (ShouldSkip(httpContext.Request, httpContext.Response))
+                    {
+                        if (debug)
+                        {
+                            Console.WriteLine("Skipping the event");
+                        }
+
+                        await responseBody.CopyToAsync(originalBodyStream);
+                    }
+                    else
+                    {
+                        if (debug)
+                        {
+                            Console.WriteLine("Calling the API to send the event to Moesif");
+                        }
+
+                        //Send event to Moesif async
+                        Middleware(request, response);
+
+                        await responseBody.CopyToAsync(originalBodyStream);
+                    }
+                }
+                else
+                {
+                    if (debug)
+                    {
+                        Console.WriteLine("Calling the API to send the event to Moesif");
+                    }
+
+                    //Send event to Moesif async
+                    Middleware(request, response);
+
+                    await responseBody.CopyToAsync(originalBodyStream);
+                }
+            }
+        }
+
+        private async Task<EventRequestModel> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+
+            request.EnableRewind();
+
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            request.Body = body;
+
+            var reqHeaders = new Dictionary<string, string>();
+            try
+            {
+                reqHeaders = request.Headers.ToDictionary(k => k.Key, k => k.Value.ToString());
+            }
+            catch (Exception inst)
+            {
+                if (debug)
+                {
+                    Console.WriteLine("error encountered while copying request header");
+                    Console.WriteLine(inst);
+                }
+            }
+
+            var reqBody = new object();
+            reqBody = null;
+            try
+            {
+                reqBody = ApiHelper.JsonDeserialize<object>(bodyAsText);
+            }
+            catch (Exception inst)
+            {
+                Console.WriteLine("error encountered while trying to serialize request body");
+                Console.WriteLine(inst);
+            }
+
+            string ip = null;
+            try
+            {
+                ip = request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            catch (Exception inst)
+            {
+                Console.WriteLine("error encountered while trying to get the client IP address");
+                Console.WriteLine(inst);
+            }
+
+            var uri = new Uri(request.GetDisplayUrl()).ToString();
+
+            string apiVersion = null;
+            var apiVersion_out = new object();
+            var getApiVersion = moesifOptions.TryGetValue("ApiVersion", out apiVersion_out);
+            if (getApiVersion)
+            {
+                apiVersion = apiVersion_out.ToString();
+            }
+
+            var eventReq = new EventRequestModel()
+            {
+                Time = DateTime.UtcNow,
+                Uri = uri,
+                Verb = request.Method,
+                ApiVersion = apiVersion,
+                IpAddress = ip,
+                Headers = reqHeaders,
+                Body = reqBody
+            };
+
+            return eventReq;
+        }
+
+        private async Task<EventResponseModel> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            var rspHeaders = new Dictionary<string, string>();
+            try
+            {
+                rspHeaders = response.Headers.ToDictionary(k => k.Key, k => k.Value.ToString());
+            }
+            catch (Exception inst)
+            {
+                if (debug)
+                {
+                    Console.WriteLine("error encountered while copying response header");
+                    Console.WriteLine(inst);
+                }
+            }
+
+            var rspBody = new object();
+            rspBody = null;
+            try
+            {
+                rspBody = ApiHelper.JsonDeserialize<object>(text);
+            }
+            catch (Exception inst)
+            {
+                Console.WriteLine("error encountered while trying to serialize response body");
+                Console.WriteLine(inst);
+            }
+
+            var eventRsp = new EventResponseModel()
+            {
+                Time = DateTime.UtcNow,
+                Status = response.StatusCode,
+                Headers = rspHeaders,
+                Body = rspBody
+            };
+
+            return eventRsp;
+        }
+
+        private async Task Middleware(EventRequestModel event_request, EventResponseModel event_response)
+        {
+            var eventModel = new EventModel()
+            {
+                Request = event_request,
+                Response = event_response,
+                UserId = userId,
+                SessionToken = sessionToken,
+                Metadata = metadata
+            };
+
+            string applicationId = null;
+            var applicationId_out = new object();
+            var getApplicationId = moesifOptions.TryGetValue("ApplicationId", out applicationId_out);
+            if (getApplicationId)
+            {
+                applicationId = applicationId_out.ToString();
+            }
+
+            if (applicationId_out == null || applicationId == null)
+            {
+                Console.WriteLine("Please provide the application Id to send events to Moesif");
+            }
+            else
+            {
+                // Get Mask Event
+                var maskEvent_out = new object();
+                var getMaskEvent = moesifOptions.TryGetValue("MaskEventModel", out maskEvent_out);
+
+                // Check to see if we need to send event to Moesif
+                Func<EventModel, EventModel> MaskEvent = null;
+
+                if (getMaskEvent)
+                {
+                    MaskEvent = (Func<EventModel, EventModel>)(maskEvent_out);
+                }
+
+                // Mask event
+                if (MaskEvent != null)
+                {
+                    try
+                    {
+                        eventModel = MaskEvent(eventModel);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Can not execute MASK_EVENT_MODEL function. Please check moesif settings.");
+                    }
+                }
+
+                // Send Events
+                try
+                {
+                    var client = new MoesifApiClient(applicationId);
+
+                    await client.Api.CreateEventAsync(eventModel);
+                    if (debug)
+                    {
+                        Console.WriteLine("Event sent successfully to Moesif");
+                    }
+                }
+                catch (APIException inst)
+                {
+                    if (401 <= inst.ResponseCode && inst.ResponseCode <= 403)
+                    {
+                        Console.WriteLine("Unauthorized access sending event to Moesif. Please check your Appplication Id.");
+                    }
+                    if (debug)
+                    {
+                        Console.WriteLine("Error sending event to Moesif, with status code:");
+                        Console.WriteLine(inst.ResponseCode);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,125 @@
+﻿# Moesif Middleware for C#
+
+Middleware to capture _incoming_ API calls and send to the Moesif API Analytics platform.
+
+[Source Code on GitHub](https://github.com/Moesif/moesif-csharp)
+
+[Nuget Package](https://www.nuget.org/packages/Moesif.Middleware/)
+
+## How to install
+
+Install the Nuget Package:
+
+```bash
+    Install-Package Moesif.Middleware
+```
+
+## How to use
+
+In `Startup.cs` file in your project directory, please add `app.UseMiddleware<MoesifMiddleware>(moesifOptions);` to the pipeline.
+
+Because of middleware execution order, it is best to add middleware __below__ SessionMiddleware
+and AuthenticationMiddleware, because they add useful session data that enables deeper error analysis. On the other hand, if you have other middleware that modified response before going out, you may choose to place Moesif middleware __above__ the middleware modifying response. This allows Moesif to see the modifications to the response data and see closer to what is going over the wire.
+
+Add the middleware to your application:
+
+```csharp
+app.UseMiddleware<MoesifMiddleware>(moesifOptions);
+```
+
+Also, add moesifOptions to your settings file,
+
+```
+moesifOptions = {
+    'ApplicationId': 'Your Application ID Found in Settings on Moesif',
+    ...
+    # other options see below.
+}
+```
+
+You can find your Application Id from [_Moesif Dashboard_](https://www.moesif.com/) -> _Top Right Menu_ -> _App Setup_
+
+## Configuration options
+
+#### __`ApplicationId`__
+(__required__), _string_, is obtained via your Moesif Account, this is required.
+
+#### __`Skip`__
+(optional) _(HttpRequest, HttpResponse) => boolean_, a function that takes a request and a response, and returns true if you want to skip this particular event.
+
+#### __`IdentifyUser`__
+(optional) _(HttpRequest, HttpResponse) => string_, a function that takes a request and a response, and returns a string that is the user id used by your system. While Moesif identify users automatically, if your set up is very different from the standard implementations, it would be helpful to provide this function.
+
+#### __`GetSessionToken`__
+(optional) _(HttpRequest, HttpResponse) => string_, a function that takes a request and a response, and returns a string that is the session token for this event. Again, Moesif tries to get the session token automatically, but if you setup is very different from standard, this function will be very help for tying events together, and help you replay the events.
+
+#### __`GetMetadata`__
+(optional) _(HttpRequest, HttpResponse) => dictionary_, getMetadata is a function that returns an object that allows you
+to add custom metadata that will be associated with the event. The metadata must be a dictionary that can be converted to JSON. For example, you may want to save a VM instance_id, a trace_id, or a tenant_id with the request.
+
+#### __`MaskEventModel`__
+(optional) _(EventModel) => EventModel_, a function that takes an EventModel and returns an EventModel with desired data removed. Use this if you prefer to write your own mask function. The return value must be a valid EventModel required by Moesif data ingestion API. For details regarding EventModel please see the [Moesif CSharp API Documentation](https://www.moesif.com/docs/api?csharp#).
+
+#### __`ApiVersion`__
+(optional), _string_, api version associated with this particular event.
+
+#### __`LocalDebug`__
+_boolean_, set to True to print internal log messages for debugging SDK integration issues.
+
+### Example:
+
+```csharp
+public static Func<HttpRequest, HttpResponse, string> IdentifyUser = (HttpRequest req, HttpResponse res) =>  {
+    return "my_user_id";  
+} ;
+
+public static Func<HttpRequest, HttpResponse, string> GetSessionToken = (HttpRequest req, HttpResponse res) => {
+    return "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f";
+};
+
+public static Func<HttpRequest, HttpResponse, Dictionary<string, string>> GetMetadata = (HttpRequest req, HttpResponse res) => {
+    Dictionary<string, string> metadata = new Dictionary<string, string>
+    {
+        { "email", "abc@email.com" },
+        { "name", "abcdef" },
+        { "image", "123" }
+    };
+    return metadata;
+};
+
+public static Func<HttpRequest, HttpResponse, bool> Skip = (HttpRequest req, HttpResponse res) => {
+    string uri = new Uri(req.GetDisplayUrl()).ToString();
+    if (uri.Contains("test"))
+    {
+        return true;
+    }
+    return false;
+};
+
+public static Func<EventModel, EventModel> MaskEventModel = (EventModel event_model) => {
+    event_model.UserId = "masked_user_id";
+    return event_model;
+};
+
+static public Dictionary<string, object> moesifOptions = new Dictionary<string, object>
+{
+    {"ApplicationId", "Your Application ID Found in Settings on Moesif"},
+    {"LocalDebug", true},
+    {"ApiVersion", "1.0.0"},
+    {"IdentifyUser", IdentifyUser},
+    {"GetSessionToken", GetSessionToken},
+    {"GetMetadata", GetMetadata},
+    {"Skip", Skip},
+    {"MaskEventModel", MaskEventModel}
+};
+
+```
+
+## Example
+
+An example Moesif integration based on quick start tutorial:
+[Moesif CSharp Example](https://github.com/Moesif/moesif-csharp-example)
+
+## Other integrations
+
+To view more more documentation on integration options, please visit __[the Integration Options Documentation](https://www.moesif.com/docs/getting-started/integration-options/).__
