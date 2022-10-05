2c90ac3bc2bd1e85c18dc66d8ba0a358c792e8aa
Praveen Kumar <praveen@moesif.com>
2022-10-04 19:58:18 -0700

    Bump up version to publish correct build

--- Moesif.Middleware/NetCore/MoesifMiddlewareNetCore.cs
+++ Moesif.Middleware/NetCore/MoesifMiddlewareNetCore.cs
@@ -91,7 +91,7 @@ namespace Moesif.Middleware.NetCore
             {
                 // Initialize client
                 debug = LoggerHelper.GetConfigBoolValues(moesifOptions, "LocalDebug", false);
-                client = new MoesifApiClient(moesifOptions["ApplicationId"].ToString(), "moesif-netcore/1.3.18", debug);
+                client = new MoesifApiClient(moesifOptions["ApplicationId"].ToString(), "moesif-netcore/1.3.19", debug);
                 logBody = LoggerHelper.GetConfigBoolValues(moesifOptions, "LogBody", true);
                 _next = next;
                 config = AppConfig.getDefaultAppConfig();
