7df20ba56dd2d846ca574063cc65bd69d1eef3fc
Praves <praveen@moesif.com>
2023-07-24 11:36:07 -0700

    #185465572: Upgrade SDK to latest 2.0.8 (#76)

--- Moesif.Middleware/Properties/AssemblyInfo.cs
+++ Moesif.Middleware/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Moesif, Inc")]
 [assembly: AssemblyProduct("Moesif.Middleware")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // Version information for an assembly consists of the following four values:
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.23")]
-[assembly: AssemblyFileVersion("1.3.23")]
+[assembly: AssemblyVersion("1.3.24")]
+[assembly: AssemblyFileVersion("1.3.24")]
